a788f4f6881ffd79266aa105860eb3254ba26f1b
Cristian fabiano Eisenhut <cristiancfe@gmail.com>
2024-02-19 21:39:25 -0300

    jogo da velha

---
+++ app.js
@@ -0,0 +1,66 @@
+
+
+
+function zero1(){
+    document.getElementById("imagem1").src = "./img/zero.jpg";
+   }
+function xis1(){
+    document.getElementById("imagem1").src = "./img/x.png";          
+
+}
+function zero2(){
+    document.getElementById("imagem2").src = "./img/zero.jpg";
+   }
+function xis2(){
+    document.getElementById("imagem2").src = "./img/x.png";          
+
+}
+function zero3(){
+    document.getElementById("imagem3").src = "./img/zero.jpg";
+   }
+function xis3(){
+    document.getElementById("imagem3").src = "./img/x.png";          
+
+}
+function zero4(){
+    document.getElementById("imagem4").src = "./img/zero.jpg";
+   }
+function xis4(){
+    document.getElementById("imagem4").src = "./img/x.png";          
+
+}
+function zero5(){
+    document.getElementById("imagem5").src = "./img/zero.jpg";
+   }
+function xis5(){
+    document.getElementById("imagem5").src = "./img/x.png";          
+
+}
+function zero6(){
+    document.getElementById("imagem6").src = "./img/zero.jpg";
+   }
+function xis6(){
+    document.getElementById("imagem6").src = "./img/x.png";          
+
+}
+function zero7(){
+    document.getElementById("imagem7").src = "./img/zero.jpg";
+   }
+function xis7(){
+    document.getElementById("imagem7").src = "./img/x.png";          
+
+}
+function zero8(){
+    document.getElementById("imagem8").src = "./img/zero.jpg";
+   }
+function xis8(){
+    document.getElementById("imagem8").src = "./img/x.png";          
+
+}
+function zero9(){
+    document.getElementById("imagem9").src = "./img/zero.jpg";
+   }
+function xis9(){
+    document.getElementById("imagem9").src = "./img/x.png";          
+
+}
